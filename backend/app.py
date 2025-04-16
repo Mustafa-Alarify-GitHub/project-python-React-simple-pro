f4cdc239e438b062d9eb95d651aae246e0027fe1
Mustafa-Alarify <m2024u775516215@gmail.com>
2025-04-16 12:19:32 +0300

    Refactor app.py to organize route imports and database initialization

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+# Routes
+import routes
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+#  set FLASK_APP=app.py
+# set FLASK_ENV = development
+# flask run --reload
