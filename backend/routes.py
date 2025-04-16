1ee673588c4499291ea702c0136ddf3ff775a2e9
Mustafa-Alarify <m2024u775516215@gmail.com>
2025-04-16 12:19:52 +0300

    Add routes for getting and creating friends in routes.py

---
+++ backend/routes.py
@@ -0,0 +1,38 @@
+from app import app, db
+from models import Friend
+from flask import jsonify, request
+
+#  Get All Friends
+
+
+@app.route('/api/friends', methods=['GET'])
+def get_friends():
+    friends = Friend.query.all()
+    return jsonify([friend.to_json() for friend in friends])
+
+# Create a New Friend
+
+
+@app.route('/api/friends', methods=['POST'])
+def create_friend():
+    try:
+        data = request.json
+        new_friend = Friend(
+            name=data.get('name'),
+            role=data.get('role'),
+            description=data.get('description'),
+            gender=data.get('gender'),
+            img_url=data.get('imgUrl')
+        )
+        db.session.add(new_friend)
+        db.session.commit()
+        return jsonify({
+            "status": 200,
+            "message": "Friend created successfully",
+            "data": new_friend.to_json()
+        })
+    except Exception as e:
+        return jsonify({
+            "message": "Error creating friend",
+            "error": str(e)
+        }), 500
